7d9859792ed428cd0c527e4dbf6adbfc98e0bd00
neistop <neistopx@gmail.com>
2024-04-27 19:36:25 +0600

    Баннер

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,75 @@
     <div class="container-iscor">
         <div class="fs-24-768 text-[64px] font-medium text-center mx-auto mt-5">СЕРВИС ПРОВЕРКИ КОНТРАГЕНТОВ И СОТРУДНИКОВ ПО КРЕДИТНОЙ ИСТОРИИ</div>
         {{--        <button class="btn-transparent w-96 w-full-768 block mx-auto mt-5">Получить полный доступ</button>--}}
-        <img class="mx-auto mt-10 mb-12" src="img/home-img.png" alt="home-img">
-        @include('components.services')
+        <div class="banner font-medium text-white flex my-10 hidden-1024">
+            <div class="w-1/2">
+                <div class="text-[32px] text-center">Проверка физического<br>или юридического лица</div>
+                <div class="text-[20px]">
+                    <div class="flex mt-10">
+                        <div class="w-2/5">Кредитный рейтинг</div>
+                        <div class="w-3/5">от 60 ₽ (физ.лицо) 380₽ (юр.лицо)</div>
+                    </div>
+                    <div class="flex mt-5">
+                        <div class="w-2/5">Скоринговый балл</div>
+                        <div class="w-3/5">от 30 ₽</div>
+                    </div>
+                </div>
+                <div class="mt-10 flex justify-center">
+                    <button class="btn-white w-2/3">Заключить договор</button>
+                </div>
+            </div>
+            <div class="w-1/2 flex flex-col justify-between">
+                <div class="text-[32px] text-center">Результат проверки в<br>течении 15-60 секунд</div>
+                <div class="mt-10 flex justify-center">
+                    <button class="btn-white w-2/3">Личный кабинет</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="w-full font-medium text-white my-5 hidden block-1024">
+        <div class="banner-mobile-1">
+            <div class="text-[20px] text-center">Проверка физического или юридического лица</div>
+            <div class="text-[14px]">
+                <div class="mt-5">
+                    <div>Кредитный рейтинг</div>
+                    <div class="mt-1">от 60 ₽ (физ.лицо) 380₽ (юр.лицо)</div>
+                </div>
+                <div class="mt-5">
+                    <div>Скоринговый балл</div>
+                    <div class="mt-1">от 30 ₽</div>
+                </div>
+            </div>
+            <button class="btn-white w-full mt-5">Заключить договор</button>
+        </div>
+        <div class="banner-mobile-2">
+            <div class="text-[20px] text-center">Результат проверки в течении 15-60 секунд</div>
+            <button class="btn-white w-full mt-5">Личный кабинет</button>
+        </div>
+    </div>
+    <div class="container-iscor">
+        <div class="text-[32px] fs-20-768 font-medium">Услуги <span class="text-green">iScor.ru</span></div>
+        <div class="flex flex-wrap flex-nowrap-768 justify-between justify-normal-768 overflow-x-auto-768 no-scrollbar mt-3">
+            <div class="card-home-services cursor-pointer mt-3" onclick="location='{{ route('credit-rating-physical-person') }}'">
+                <img class="w-12 h-12" src="img/services-1.svg" alt="">
+                <div class="text-2xl font-semibold mt-3">КРЕДИТНЫЙ РЕЙТИНГ ФИЗ.ЛИЦО</div>
+                <div class="text-sm mt-3">Основные показатели кредитной истории. Для запроса необходимо Ф.И.О. серия, номер паспорта</div>
+            </div>
+            <div class="card-home-services cursor-pointer mt-3" onclick="location='{{ route('credit-rating-legal-entity') }}'">
+                <img class="w-12 h-12" src="img/services-2.svg" alt="">
+                <div class="text-2xl font-semibold mt-3">КРЕДИТНЫЙ РЕЙТИНГ ЮР.ЛИЦО</div>
+                <div class="text-sm mt-3">Основные показатели кредитной истории. Для запроса необходим ИНН</div>
+            </div>
+            <div class="card-home-services cursor-pointer mt-3" onclick="location='{{ route('scoring') }}'">
+                <img class="w-12 h-12" src="img/services-3.svg" alt="">
+                <div class="text-2xl font-semibold mt-3">СКОРИНГОВЫЙ БАЛЛ</div>
+                <div class="text-sm mt-3">Оценка кредиспособности и платежной дисциплины. Упрощенный сбор данных для запроса. Ф.И.О. и дата рождения.</div>
+            </div>
+            <div class="card-home-services cursor-pointer mt-3" onclick="location='{{ route('api-integration') }}'">
+                <img class="w-12 h-12" src="img/services-5.svg" alt="">
+                <div class="text-2xl font-semibold mt-3">API ИНТЕГРАЦИЯ</div>
+                <div class="text-sm mt-3">Внедрение проверки кредитной истории непосредственно в вашу систему, с фокусированием на особо важных аспектах.</div>
+            </div>
+        </div>
     </div>
     <div class="bg-green text-white mt-20 py-10">
         <div class="container-iscor">
@@ -118,7 +185,7 @@
                 <button class="btn-arrow-green" onclick="location='{{ route('rates') }}'">Все тарифы</button>
             </div>
             <div class="flex flex-wrap flex-nowrap-768 justify-between justify-normal-768 overflow-x-auto-768 no-scrollbar mt-3">
-                <div class="card-rates mt-3">
+                <div class="card-home-rates mt-3">
                     <img class="w-24px-768 ml-auto" src="img/rates-1.svg" alt="">
                     <div class="text-[24px] fs-16-768 font-semibold">Базовый</div>
                     <div class="text-[24px] fs-20-768 font-medium font-semibold-768">52 000 ₽/год</div>
@@ -142,7 +209,7 @@
                         <button class="btn-transparent w-full mt-5">Купить</button>
                     </a>
                 </div>
-                <div class="card-rates mt-3">
+                <div class="card-home-rates mt-3">
                     <img class="w-24px-768 ml-auto" src="img/rates-1.svg" alt="">
                     <div class="text-[24px] fs-16-768 font-semibold">Комфортный</div>
                     <div class="text-[24px] fs-20-768 font-medium font-semibold-768">174 000 ₽/год</div>
@@ -166,7 +233,7 @@
                         <button class="btn-transparent w-full mt-5">Купить</button>
                     </a>
                 </div>
-                <div class="card-rates mt-3">
+                <div class="card-home-rates mt-3">
                     <img class="w-24px-768 ml-auto" src="img/rates-1.svg" alt="">
                     <div class="text-[24px] fs-16-768 font-semibold">Выгодный</div>
                     <div class="text-[24px] fs-20-768 font-medium font-semibold-768">338 000 ₽/год</div>
